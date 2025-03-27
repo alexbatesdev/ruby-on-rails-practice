37fc12c1ca3838d624639178759379f71faf9250
Alex Bates <alexbates.dev@gmail.com>
2025-03-26 21:23:00 -0600

    secret fetching untested, weather api call basically just pseudo code

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,4 +1,11 @@
 class WeatherController < ApplicationController
   def index
+    city = params[:city] || "New York"
+    api_key = ENV["OPEN_WEATHER_KEY"]
+    # instead of this, get lat and lon from geo api using city value
+    # https://openweathermap.org/api/geocoding-api
+    lat = params[:lat] || "40.7128"
+    lon = params[:lon] || "-74.0060"
+    url = "http://api.openweathermap.org/data/3.0/weather?lat=#{lat}&lon=#{lon}&appid=#{api_key}"
   end
 end
